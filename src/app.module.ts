495309dc2d4e25d36de416e5d28e49e3d5f2e9b7
Nikita Osminin <nikita.karpinskiy@mail.ru>
2026-01-19 10:38:43 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [CatsModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/cats/dto/create-cat.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateCatDto {
+  @ApiProperty()
+  name: string;
+  @ApiProperty()
+  age: number;
+}
